ad87cfa56632761e236eeda776a5698ee83bfcde
DemanC <demanchenmian@163.com>
2023-09-28 14:59:53 +0800

    style: code prettier

--- components.d.ts
+++ components.d.ts
@@ -7,10 +7,10 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
-    SvgIcon: typeof import('./src/components/SvgIcon.vue')['default']
+    ElButton: (typeof import('element-plus/es'))['ElButton']
+    ElConfigProvider: (typeof import('element-plus/es'))['ElConfigProvider']
+    RouterLink: (typeof import('vue-router'))['RouterLink']
+    RouterView: (typeof import('vue-router'))['RouterView']
+    SvgIcon: (typeof import('./src/components/SvgIcon.vue'))['default']
   }
 }
